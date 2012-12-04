4291744a3a76fe9bc572ad634796da3fa54e37b7
HUO Zhuoxi <huo@huo-w520.(none)>
2012-12-04 12:32:04 +0800

    first commit

---
+++ sigmaclipping.m
@@ -0,0 +1,21 @@
+function sigma=sigmaclipping(x,k)
+%SIGMACLIPPING
+% Reference:
+% J.-L. Starck and F. Murtagh, Astronomical Image and Data Analysis, 2nd
+% Edition, Springer, p40-p41
+if nargin<2
+    k=3;
+end
+MAX_LOOP=20;
+x=x(:);
+sigma=std(x(:));
+for n=1:MAX_LOOP
+    l=logical(abs(x)<=(k*sigma));
+    sigma_new=std(x(l));
+    if sigma_new==sigma
+        return
+    end
+    sigma=sigma_new;
+end
+disp('Iteration limit reached without convergence.');
+return
